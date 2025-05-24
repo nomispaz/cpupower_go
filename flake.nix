f06fa7f618c874a879ab178519b897f03fdabdf3
nomispaz <111981114+nomispaz@users.noreply.github.com>
2025-05-24 10:07:57 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       src = pkgs.fetchFromGitHub {
         owner = "nomispaz";
         repo = "cpupower_go";
-        rev = "81cd4258a1996eb3574c7f4ab508184829398a07";
+        rev = "52b306757c6b138b65e4ca9a77f56950b95107df";
         sha256 = "sha256-rkk8oH5ShfEoGyVVSHxgBWwAcF5jlYghZRcsVdrw/4s=";  # for the source archive
       };
 
