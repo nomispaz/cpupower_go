fc359da562d846a1efd1a94769fbca49da2aaca7
nomispaz <nomispaz@email>
2025-05-24 09:43:14 +0200

    added flake for nix

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Flake for cpupower_go";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system}.cpupower_go = pkgs.buildGoModule {
+      pname = "cpupower_go";
+      version = "0.1.0";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "nomispaz";
+        repo = "cpupower_go";
+        rev = "81cd4258a1996eb3574c7f4ab508184829398a07";
+        sha256 = "sha256-HaMgPqRw/NV3ar9X1XRhfVYgP8H3jeroilCVUJH9uRs=";  # for the source archive
+      };
+
+      vendorHash = "sha256-W7Aj/Jfr4AgAGYW9TdTUEmUqtWDb1G1kI/LUP6pFNNk=";  # temp hash to get the real one
+
+      preBuild = ''
+        export GOWORK=off
+      '';
+
+      meta = {
+        description = "Simple wrapper writte in go for cpupower";
+        homepage = "https://github.com/nomispaz/cpupower";
+        license = pkgs.lib.licenses.gpl3;
+      };
+    };
+
+    defaultPackage.${system} = self.packages.${system}.cpupower_go;
+  };
+}
