dd4d88b6ee9ca3b89136d2990aefb17cbccda1a3
nomispaz <111981114+nomispaz@users.noreply.github.com>
2025-05-25 22:29:35 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,22 +7,12 @@ import (
 	"time"
 )
 
-func change_freq(freq string)  {
-	for true {
-		cmd := exec.Command("bash", "-c", "sudo cpupower frequency-set -u " + freq)
-		cmd.Run()
-		time.Sleep(2 * time.Second)
-	}
-}
-
 func main() {
 	fmt.Println("Setting the cpu frequency to " + os.Args[1] + " via cpupower frequency-set")
 	fmt.Println("The command is executed every 2 seconds since cpupower with amd doesn't persist the set freuquency.")
-
-	channel := make(chan string)
-	
-	go change_freq(os.Args[1])
-
-	close(channel)
-
+	for true {
+		cmd := exec.Command("bash", "-c", "sudo cpupower frequency-set -u " + os.Args[1])
+		cmd.Run()
+		time.Sleep(2 * time.Second)
+	}
 }
